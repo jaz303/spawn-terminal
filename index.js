6c2cb187319344822d3b0405f17539f72b289d49
Jason Frame <jason@onehackoranother.com>
2013-07-30 11:40:42 +0100

    tidy up

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 var spawn = require('child_process').spawn;
 
-var spawnTerminal = null;
-
 function home() {
   return process.env[(process.platform === 'win32') ? 'USERPROFILE' : 'HOME'];
 }
@@ -10,43 +8,38 @@ function escapeDirName(dir) {
   return "'" + dir.replace('~', home()) + "'";
 }
 
-if (process.platform === 'darwin') {
-
-  spawnTerminal = function(dir, cb) {
-    
-    if (typeof dir === 'function') {
-      cb = dir;
-      dir = null;
-    }
-
-    dir = dir || home();
-
-    var child = spawn('osascript', ['-e', 'tell application "terminal"',
-                                    '-e', 'do script "cd ' + escapeDirName(dir) + '"',
-                                    '-e', 'end tell']);
-    if (cb) {
-      child.on('exit', function() { cb(dir); });
-    }
+function spawnTerminal(dir, cb) {
   
+  if (typeof dir === 'function') {
+    cb = dir;
+    dir = null;
+  }
+
+  dir = dir || home();
+  
+  var child = doSpawn(dir);
+  
+  if (cb) {
+    child.on('exit', function() { cb(dir); });
+  }
+  
+}
+
+var doSpawn = null;
+
+if (process.platform === 'darwin') {
+  
+  doSpawn = function(dir) {
+    return spawn('osascript', ['-e', 'tell application "terminal"',
+                               '-e', 'do script "cd ' + escapeDirName(dir) + '"',
+                               '-e', 'end tell']);
   }
 
 } else if (process.platform === 'linux') {
-
-  spawnTerminal = function(dir, cb) {
-
-    if (typeof dir === 'function') {
-      cb = dir;
-      dir = null;
-    }
-
-    dir = dir || home();
-
-    var child = spawn('gnome-terminal', ['--working-directory=' + escapeDirName(dir)]);
-
-    if (cb) {
-      child.on('exit', function() { cb(dir); });
-    }
-
+  
+  doSpawn = function(dir) {
+    // TODO: pick correct terminal
+    return spawn('gnome-terminal', ['--working-directory=' + escapeDirName(dir)]);
   }
 
 } else {
